21d615d11fd9c6e84613e998b0e01355108d9f8c
Alex <alexandersuglio@gmail.com>
2017-06-17 19:52:22 -0400

    added connection string for jawsDB

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 var mySQL = require('mysql');
 var connection;
 
-if ('mysql://pxonzzsbvbd7spvy:ec64nx7eu0n6n2q6@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jmxb88i1v6vgoadd'
+if ('mysql://mqrs75wut4ubcx6o:vsj57p27dcl42e90@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dlgwn6j0uhg770o9'
 
 ) {
 
-    connection = mySQL.createConnection('mysql://pxonzzsbvbd7spvy:ec64nx7eu0n6n2q6@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jmxb88i1v6vgoadd'
+    connection = mySQL.createConnection('mysql://mqrs75wut4ubcx6o:vsj57p27dcl42e90@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dlgwn6j0uhg770o9'
 
     );
 
