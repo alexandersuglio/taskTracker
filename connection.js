5954a48497aef7ca18e61476312772a3a0afa6d9
Alex <alexandersuglio@gmail.com>
2017-06-17 17:25:03 -0400

    added Heroku connection string

--- connection.js
+++ connection.js
@@ -1,12 +1,28 @@
 var mySQL = require('mysql');
-var connection = mySQL.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'taskDB'
-});
+var connection;
+
+if ('mysql://woop6poz6mshqfa3:ygmhrnz9vcutkenp@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n0w9uutyinke4xg9'
+
+) {
+
+    connection = mySQL.createConnection('mysql://woop6poz6mshqfa3:ygmhrnz9vcutkenp@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n0w9uutyinke4xg9'
+
+    );
+
+
+} else {
+
+    connection = mySQL.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'taskDB'
+    });
+
+
+};
+
+
 connection.connect();
 module.exports = connection;
-
-
